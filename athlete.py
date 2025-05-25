7b8398c964e7c896604cfa8512dbb33411ba212d
josediaz99 <josediazisnow@gmail.com>
2025-05-25 13:51:09 -0500

    modulerizing the steps reqwuired to scrape lifting cast

---
+++ athlete.py
@@ -0,0 +1,114 @@
+class athlete():
+    '''
+    a class created to store athlete information requiring name,team, and sex to be able to accurately represent them
+    due to weight being on a different table on lifting cast we cant have weight on init
+    
+    '''
+       
+    def __init__(self,name,team,sex):
+        self.name = name
+        self.team = team
+        self.isMale = sex
+        self.Class = 0
+        self.weight = 0
+        self.total = 0
+        self.gl = 0
+        self.bestSquat = 0
+        self.bestDead = 0
+        self.bestBench = 0
+        
+    def setWeight(self,kgs):
+        '''
+        sets athletes weight and places them in their weight class
+        '''
+        self.weight = kgs
+        self.setClass(kgs)
+        
+    def getWeight(self):
+        '''
+        returns the athletes bodyweight at weight ins
+        '''
+        return self.weight
+    
+    def setClass(self,kgs):
+        '''
+        sets athlete weight class depending on sex and weight in kgs
+        if class will be set to high number for heavy weights
+        
+        o(n) time
+        '''
+        found = False
+        paMClasses = [59,66,74,83,93,105,120,999]
+        paFClasses = [47,52,57,63,72,84,998]
+        weight = 0
+        
+        if self.isMale:
+            while weight < len(paMClasses) and not found:
+                if paMClasses[weight] > kgs:
+                    found = True
+                    weight = paMClasses[weight]
+                else:
+                    weight += 1
+        else:#if theyre a women
+            while weight < len(paFClasses) and not found:
+                if paFClasses[weight] > kgs:
+                    found = True
+                    weight = paFClasses[weight]
+                else:
+                    weight += 1
+        
+        self.Class = weight
+        
+    def getClass(self):
+        '''
+        returns the athlete weight class
+        '''
+        return self.Class
+    
+    def getTotal(self):
+        '''
+        returns the total of the athlete
+        '''
+        total = self.bestBench+self.bestDead+self.bestSquat
+        return total
+    
+    def updateBest(self,lift,attempt,isGood):
+        """
+        will change the bestlift depending on the lift and if the attempt was made
+        Args:
+            lift (str): describes what kind of lift is being made
+            attempt (int): weight of the attempt
+            isGood (bool): if the attempt was made
+        """
+        if isGood:
+            match lift:
+                case "dead":
+                    self.bestDead = attempt
+                case "bench":
+                    self.bestBench = attempt
+                case "squat":
+                    self.bestSquat = attempt
+                
+            self.calcGlPts()
+            
+    def calcGlPts(self):
+        weight = self.weight
+        total = self.total
+        sex = self.isMale
+        a,b,c = 0
+        
+        if sex:
+            a = 1199.72839
+            b = -102.11299
+            c = 0.0000022
+        else:
+            a = 1249.1533
+            b = -123.9606
+            c = 0.0000062
+            
+        bottom = a+b * weight + c * weight**2
+        glPoints = (total/bottom) * 100
+        self.gl = glPoints
+        
+    def getGl(self):
+        return self.gl
---
+++ liftCastCompScrape.py
@@ -0,0 +1,101 @@
+import asyncio
+from crawl4ai import AsyncWebCrawler
+from bs4 import BeautifulSoup
+from scrapeLiftcastUrl import scrapeResult
+
+'''
+performs datascraping on the initial lifting cast page and enters the competition which is selected
++ stores a list of competitions which match the soonest date
++ stores a list of athletes the selected competition
+  - allows to add/remove athletes to your team
+  - stores athlete static stats (weightClass,weight,team,age)
+'''
+async def fetchEvents():
+    '''
+    this function gets the raw html from liftingcast
+    we then parse the html using beautifulSoup to find the table with the information we want
+    
+    returns a list of dictionaries (compName,date,link)
+    '''
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(url="https://liftingcast.com/")
+        html = result.html
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # find the element that matches the list table name were looking for
+    anchor = soup.find('a', id='upcoming-meets')
+    if not anchor:
+        print("Couldn't find the Upcoming Meets anchor.")
+        return
+
+    # using the anchor find the table that contains the element
+    table = anchor.find_parent('table')
+    if not table:
+        print("Couldn't find a parent <table> for Upcoming Meets.")
+        return
+
+    # gets only the body portion of the table
+    tbody = table.find('tbody')
+    if not tbody:
+        print("No <tbody> in that table.")
+        return
+
+    rows = tbody.find_all('tr')  #gets every row
+    
+    events = []
+    for row in rows:
+        cells = row.find_all('td')
+        if len(cells) < 2:
+            continue
+
+        a = cells[0].find('a', href=True)#gets the link in the href of the a element in the left cell
+        name = a.get_text(strip=True)
+        link = a['href']
+        # transform it into the full link
+        full_link = f"https://liftingcast.com{link}"
+
+        date = cells[1].get_text(strip=True)#gets the data stored on the right cell of the element
+        events.append({
+            'name': name,
+            'date': date,
+            'link': full_link
+        })
+
+    # gets the first date and transforms the list that match the first date
+    if events:
+        first_date = events[0]['date']
+        events = [e for e in events if e['date'] == first_date]
+
+    return events
+        
+def getEvent(events):
+    """displays the events in our list and returns the link of the chosen comp
+
+    Args:
+        events (list): list of dictionaries wich contain comp name, date , and link
+
+    Returns:
+        str: the string for the link of the specified competition
+    """
+    for i,event in enumerate(events):
+        print(i,event['name'])
+    
+    comp = input("which competition(using the #): ")
+    comp = int(comp)
+    print("loading...")
+    return events[comp]['link']
+
+async def main():
+    events = await fetchEvents()
+    compUrl = getEvent(events)
+    print(compUrl)
+    
+if __name__ == '__main__':
+    asyncio.run(main())
+    
+    
+    
+
+   
+    
