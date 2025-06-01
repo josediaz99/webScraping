b6247217e60ae27e325eaa24d12f4c2d738e3826
jose diaz <josediazisnow@gmail.com>
2025-06-01 17:27:19 -0500

    we created a login page following a tutorial which will be refactored to allow coaches to be able to access their information after leaving the website so information doesnt need to be scaped each time

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from website import create_app
 from liftCastCompScrape import fetchEvents,getEvent
 from scrapeAthletes import getAthletes
-from athlete import athlete
+
+app = create_app()
 
 def main():
     events = fetchEvents()                  # get the events from lifticast
     comp = getEvent(events)                 # get specific comp link from user
     boardUrl,athletes = getAthletes(comp)   # get athlete information from url
-    
+
+if __name__ == "__main__":
+    app.run(debug=True)
